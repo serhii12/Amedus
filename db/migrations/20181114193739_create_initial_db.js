92bffdfc9bc56ae1f91cd998264213b467a01d7b
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-11-14 21:39:45 +0000

    Created the dbs

---
+++ db/migrations/20181114193739_create_initial_db.js
@@ -0,0 +1,37 @@
+exports.up = async function(knex, Promise) {
+
+  await Promise.all([
+    knex.schema.createTable('item', function(table){
+      table.increments('id').unsigned().primary();
+      table.text('name');
+      table.text('description');
+      table.integer('price');
+      table.text('section');
+    }),
+
+    knex.schema.createTable('order', function(table){
+      table.increments('id').unsigned().primary();
+      table.text('phone_number');
+      table.dateTime("order_date");
+      table.dateTime("status");
+    }),
+
+  ])
+
+  await knex.schema.createTable('orderitem', function(table){
+          table.increments('id').unsigned().primary();
+          table.integer('order_id').references('order.id');
+          table.integer('item_id').references('item.id');
+          table.integer('price');
+          table.integer('quantity');
+    })
+};
+
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTable('orderitem');
+  await knex.schema.dropTable('item');
+  await knex.schema.dropTable('order');
+};
+
+
+
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,11 +1,83 @@
 exports.seed = function(knex, Promise) {
-  return knex('users')
+  return knex('item')
     .del()
     .then(() =>
       Promise.all([
-        knex('users').insert({ id: 1, name: 'Alice' }),
-        knex('users').insert({ id: 2, name: 'Bob' }),
-        knex('users').insert({ id: 3, name: 'Charlie' }),
+        knex('item').insert({
+          id: 1,
+          name: 'Hamburger',
+          price: 5.99,
+          section: "main" }),
+        knex('item').insert({
+          id: 2,
+          name: 'Cheeseburger',
+          price: 6.29,
+          section: "main" }),
+        knex('item').insert({
+          id: 3,
+          name: 'Bacon Cheeseburger',
+          price: 7.99,
+          section: "main" }),
+        knex('item').insert({
+          id: 4,
+          name: 'Chicken Strips',
+          price: 5.99,
+          section: "main" }),
+        knex('item').insert({
+          id: 5,
+          name: 'Chicken Bacon Ranch',
+          price: 8.29,
+          section: "main" }),
+        knex('item').insert({
+          id: 6,
+          name: 'Holy Smokes Burger',
+          price: 10.49,
+          section: "main" }),
+        knex('item').insert({
+          id: 7,
+          name: 'Heavvy Burger',
+          price: 8.99,
+          section: "main" }),
+        knex('item').insert({
+          id: 8,
+          name: 'Gravy',
+          price: 1.29,
+          section: "side" }),
+        knex('item').insert({
+          id: 9,
+          name: 'Fresh Cut Fries',
+          price: 5.99,
+          section: "side" }),
+        knex('item').insert({
+          id: 10,
+          name: 'Poutine',
+          price: 6.99,
+          section: "side" }),
+        knex('item').insert({
+          id: 11,
+          name: 'Cheese Fries',
+          price: 5.99,
+          section: "side" }),
+        knex('item').insert({
+          id: 12,
+          name: 'Orange Juice',
+          price: 1.99,
+          section: "drink" }),
+        knex('item').insert({
+          id: 13,
+          name: 'Pop',
+          price: 1.69,
+          section: "drink" }),
+        knex('item').insert({
+          id: 14,
+          name: 'Bottled Water',
+          price: 1.69,
+          section: "drink" }),
+        knex('item').insert({
+          id: 15,
+          name: 'Milk Shake',
+          price: 4.99,
+          section: "drink" })
       ])
     );
 };
