6a898347eac18d0f90cb0b668005b8165af212d2
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-11-14 21:57:36 +0000

    Created seeding menu

--- db/seeds/items.js
+++ db/seeds/items.js
@@ -7,75 +7,90 @@ exports.seed = function(knex, Promise) {
           id: 1,
           name: 'Hamburger',
           price: 5.99,
+          description: "Something"
           section: "main" }),
         knex('item').insert({
           id: 2,
           name: 'Cheeseburger',
+          description: "Something"
           price: 6.29,
           section: "main" }),
         knex('item').insert({
           id: 3,
           name: 'Bacon Cheeseburger',
+          description: "Something"
           price: 7.99,
           section: "main" }),
         knex('item').insert({
           id: 4,
           name: 'Chicken Strips',
+          description: "Something"
           price: 5.99,
           section: "main" }),
         knex('item').insert({
           id: 5,
           name: 'Chicken Bacon Ranch',
+          description: "Something"
           price: 8.29,
           section: "main" }),
         knex('item').insert({
           id: 6,
           name: 'Holy Smokes Burger',
+          description: "Something"
           price: 10.49,
           section: "main" }),
         knex('item').insert({
           id: 7,
           name: 'Heavvy Burger',
+          description: "Something"
           price: 8.99,
           section: "main" }),
         knex('item').insert({
           id: 8,
           name: 'Gravy',
+          description: "Something"
           price: 1.29,
           section: "side" }),
         knex('item').insert({
           id: 9,
           name: 'Fresh Cut Fries',
+          description: "Something"
           price: 5.99,
           section: "side" }),
         knex('item').insert({
           id: 10,
           name: 'Poutine',
+          description: "Something"
           price: 6.99,
           section: "side" }),
         knex('item').insert({
           id: 11,
           name: 'Cheese Fries',
+          description: "Something"
           price: 5.99,
           section: "side" }),
         knex('item').insert({
           id: 12,
           name: 'Orange Juice',
+          description: "Something"
           price: 1.99,
           section: "drink" }),
         knex('item').insert({
           id: 13,
           name: 'Pop',
+          description: "Something"
           price: 1.69,
           section: "drink" }),
         knex('item').insert({
           id: 14,
           name: 'Bottled Water',
+          description: "Something"
           price: 1.69,
           section: "drink" }),
         knex('item').insert({
           id: 15,
           name: 'Milk Shake',
+          description: "Something"
           price: 4.99,
           section: "drink" })
       ])
---
+++ db/seeds/orders.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex, Promise) {
+  return knex('order')
+    .del()
+    .then(() =>
+      Promise.all([
+        knex('order').insert({
+          id: 1,
+          phone_number: '6472234590',
+          order_date: "11-11-2018",
+          status: "complete" }),
+        knex('order').insert({
+          id: 2,
+          phone_number: '6472234590',
+          order_date: "11-11-2018",
+          status: "complete" }),
+        knex('order').insert({
+          id: 3,
+          phone_number: '6472234590',
+          order_date: "11-11-2018",
+          status: "complete" }),
+        knex('order').insert({
+          id: 4,
+          phone_number: '6472234590',
+          order_date: "11-11-2018",
+          status: "complete" }),
+      ])
+    );
+};
