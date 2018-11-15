225175a645c8bdf4cbdae3c18cce24019ca3a71d
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-11-15 19:54:30 +0000

    Updated the seed file with descriptions

--- db/seeds/1_items.js
+++ db/seeds/1_items.js
@@ -6,76 +6,76 @@ exports.seed = function(knex, Promise) {
         knex('item').insert({
           name: 'Hamburger',
           price: 599,
-          description: "Something",
+          description: "Premium beef patty topped the way you like it.",
           section: "main"}),
         knex('item').insert({
           name: 'Cheeseburger',
-          description: "Something",
+          description: "Premium beef patty with cheese.",
           price: 629,
           section: "main" }),
         knex('item').insert({
           name: 'Bacon Cheeseburger',
-          description: "Something",
+          description: "Cheeseburger topped with crisp bacon.",
           price: 799,
           section: "main" }),
         knex('item').insert({
           name: 'Chicken Strips',
-          description: "Something",
+          description: "Hand-breaded fried chicken strips served with either our G14 or BBQ sauce.",
           price: 599,
           section: "main" }),
         knex('item').insert({
           name: 'Chicken Bacon Ranch',
-          description: "Something",
+          description: "A freshly hand-breaded fried chicken sandwich with buttermilk ranch, Swiss cheese, lettuce & crispy bacon.",
           price: 829,
           section: "main" }),
         knex('item').insert({
           name: 'Holy Smokes Burger',
-          description: "Something",
+          description: "Double cheeseburger topped with panko-crusted deep-fried jalapeños.",
           price: 1049,
           section: "main" }),
         knex('item').insert({
           name: 'Heavvy Burger',
-          description: "Something",
+          description: "Secret sauce, diced onions, shredded lettuce, pickles, deep fried jalapeño smoke & bacon.",
           price: 899,
           section: "main" }),
         knex('item').insert({
           name: 'Gravy',
-          description: "Something",
+          description: "Add a complement to your meal.",
           price: 129,
           section: "side" }),
         knex('item').insert({
           name: 'Fresh Cut Fries',
-          description: "Something",
+          description: "Choice of adding gravy.",
           price: 599,
           section: "side" }),
         knex('item').insert({
           name: 'Poutine',
-          description: "Something",
+          description: "",
           price: 699,
           section: "side" }),
         knex('item').insert({
           name: 'Cheese Fries',
-          description: "Something",
+          description: "Fresh cut fries topped with shredded cheese.",
           price: 599,
           section: "side" }),
         knex('item').insert({
           name: 'Orange Juice',
-          description: "Something",
+          description: "",
           price: 199,
           section: "drink" }),
         knex('item').insert({
           name: 'Pop',
-          description: "Something",
+          description: "",
           price: 169,
           section: "drink" }),
         knex('item').insert({
           name: 'Bottled Water',
-          description: "Something",
+          description: "",
           price: 169,
           section: "drink" }),
         knex('item').insert({
           name: 'Milk Shake',
-          description: "Something",
+          description: "The name says it all.",
           price: 499,
           section: "drink" })
       ])
