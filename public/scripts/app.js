a1956c98f652bee887db4f29d7547a43af46d573
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-11-16 14:13:53 +0000

    removed empty spaces in app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,7 @@ $(() => {
     function(event) {
       if (this.hash !== '') {
         event.preventDefault();
-
         const hash = this.hash;
-
         $('html, body').animate(
           {
             scrollTop: $(hash).offset().top,
